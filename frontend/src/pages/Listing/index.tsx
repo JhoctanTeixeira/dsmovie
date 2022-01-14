0b2c00873929740a8e0c72cb2c0bceedc7157825
Jhoctan Teixeira <rm88206@fiap.com.br>
2022-01-14 13:51:34 -0300

    Up error truncate sucess

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
-import { truncate } from "fs";
 import { useEffect, useState } from "react";
 import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/resquests";
